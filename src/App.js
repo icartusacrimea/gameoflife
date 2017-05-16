d81dd99e790122585ceb40df2a3522df9e994dc2
icartusacrimea <icartusacrimea@gmail.com>
2017-05-16 02:39:06 -0400

    buttons no longer affected by transformation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var stopcount = 0;
+
 var createCellBoard = function() {
   var rows = 25,
   columns = 25,
@@ -34,10 +36,12 @@ class App extends React.Component {
     this.checkStatus = this.checkStatus.bind(this);
     this.isInGrid = this.isInGrid.bind(this);
     this.clearBoard = this.clearBoard.bind(this);
+    this.startFromStop = this.startFromStop.bind(this);
   }
 
   start() {
-    if (this.state.start === false) {
+    //initial start
+    if (this.state.start === false && this.state.stop === false) {
       this.setState({
         game: this.initRandom(),
         start: true,
@@ -45,9 +49,11 @@ class App extends React.Component {
         clear: false
       });
     }
+    if (this.state.stop === true && stopcount > 0) {
+      console.log("inside itttttt");
+    }
 
     if (this.state.stop === false) {
-      console.log(this.state.game);
       this.checkStatus();
       this.setState({
         game: this.checkStatus(),
@@ -67,6 +73,8 @@ class App extends React.Component {
       game: this.clearBoard(),
       clear: true,
       generation: 0,
+      start: false,
+      stop: false
     });
 
   }
@@ -82,6 +90,7 @@ class App extends React.Component {
   }
 
   stop() {
+    stopcount++;
     this.setState({
       stop: true
     });
@@ -100,50 +109,58 @@ class App extends React.Component {
   }
 
   checkStatus() {
-      var check = [...this.state.game];
-      var neighbors = [ [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1] ];
-
-      for (var r = 0; r < 25; r++) {
-        for (var c = 0; c < 25; c++) {
-          var cell = check[r][c];
-          cell.neighbors = 0;
-          for (var i = 0; i < neighbors.length; i++) {
-            var posit = neighbors[i];
-            var row = posit[0];
-            var col = posit[1];
-            if (this.isInGrid(r + row, col + c)) {
-              //console.log("in grid");
-              var neighbor = check[r + row][c + col];
-              if (neighbor.alive) {
-                cell.neighbors++;
-              } 
-            }
+    var check = [...this.state.game];
+    var neighbors = [ [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1] ];
+    for (var r = 0; r < 25; r++) {
+      for (var c = 0; c < 25; c++) {
+        var cell = check[r][c];
+        cell.neighbors = 0;
+        for (var i = 0; i < neighbors.length; i++) {
+          var posit = neighbors[i];
+          var row = posit[0];
+          var col = posit[1];
+          if (this.isInGrid(r + row, col + c)) {
+            //console.log("in grid");
+            var neighbor = check[r + row][c + col];
+            if (neighbor.alive) {
+              cell.neighbors++;
+            } 
           }
-          //console.log(cell.neighbors);
-          
-          //cell is alive
-          if (check[r][c].alive) {
-            if (cell.neighbors < 2 || cell.neighbors > 3) {
-              check[r][c].alive = false;
-            }
-          //cell is dead
-          } else if (check[r][c].alive === false) {
-            if (cell.neighbors === 3) {
-              check[r][c].alive = true;
-            }
+        }
+        //cell is alive
+        if (check[r][c].alive) {
+          if (cell.neighbors < 2 || cell.neighbors > 3) {
+            check[r][c].alive = false;
+          }
+        //cell is dead
+        } else if (check[r][c].alive === false) {
+          if (cell.neighbors === 3) {
+            check[r][c].alive = true;
           }
         }
       }
-      console.log(check);
+    }
     return check;
   }
 
   isInGrid(r, c) {
-    return r >= 0 && r < 25 && c >= 0 && c < 25;
+    return (r >= 0 && r < 25) && (c >= 0 && c < 25);
+  }
+
+  startFromStop() {
+   //this.checkStatus();
+   //if (this.state.start === true && this.state.stop === true) {
+    this.setState({
+        //stop: false,
+        game: this.checkStatus(),
+        generation: this.state.generation + 1
+      });
+
+      setTimeout(this.start, 500);
+   //}
   }
 
   render() {
-    //console.log(this.state.game);
     return (
       <div className="gameOfLife">
       <h4 className="conway">Conway's</h4>
@@ -152,8 +169,8 @@ class App extends React.Component {
         <button className="start" onClick={this.start}>Start</button>
         <button className="stop" onClick={this.stop}>Stop</button>
         <button className="clear" onClick={this.clear}>Clear</button>
-        </div>
         <p className="count">Generation: {this.state.generation}</p>
+        </div>
         <div className="grid">
           <div className="flex-grid-row">
             {this.state.game.map((row, i) => <CellRow key={i} index={i} gameRow={row} start={this.state.start} stop={this.state.stop} clear={this.state.clear} />)}
@@ -165,33 +182,28 @@ class App extends React.Component {
 }
 
 class CellRow extends React.Component {
-
   render() {
-      
     return (
       <div className="fadeInEach">
-      {this.props.gameRow.map((cell, i) => <Cell key={cell + 'x' + i} cell={cell} row={this.props.row} col={i} start={this.props.start} stop={this.props.stop} clear={this.props.clear} />)}
+      {this.props.gameRow.map((cell, i) => <Cell key={i} cell={cell} start={this.props.start} stop={this.props.stop} clear={this.props.clear} />)}
       </div>
     );
   }
-
 }
 
 class Cell extends React.Component {
 
   render() {
     var thiscell = this.props.cell;
-    if (!this.props.start || this.props.clear) {
+    if (this.props.start === false || this.props.clear === true) {
       return ( <div className='cell'></div> );
-    } else if (this.props.start) {
+    } else if (this.props.start === true) {
       if (thiscell.alive) {
         return ( <div className='cell true'></div> );
       } else {
         return ( <div className='cell false'></div> );
       }
     }
-    
-    //);
   }
 
 }
